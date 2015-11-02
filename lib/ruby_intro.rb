e8d69340995a7cdaf86fd1f83de76ce4ff9b9602
Nick Christ <nchrist01@gmail.com>
2015-11-02 13:42:14 +0000

    Made part 2 pass

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -38,15 +38,25 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  pattern1 = /[a-zA-Z]/ 
+  pattern2 = /[^AEIOU]/
+  pattern1 =~ s.chr.upcase and pattern2 =~ s.chr.upcase
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # Matches digits
+  #digit_pattern = /[^01]/
+  #not 2-9
+  #not_2to9 = /[^2-9]/
+  #ends in 00
+  #div_by_4 = /00$/
+  
+  #digit_pattern =~ s and not_2to9 =~ s and div_by_4 =~ s
+  /^(0*1[01]*00|0)$/ =~ s and /[^01]/
 end
 
 # Part 3
